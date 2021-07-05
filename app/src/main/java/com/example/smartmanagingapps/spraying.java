d10c7e0d0881ba1454069b8fd1256f176b5429b3
Asus <193387@student.upm.edu.my>
2021-07-05 22:14:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/smartmanagingapps/spraying.java
@@ -0,0 +1,14 @@
+package com.example.smartmanagingapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class spraying extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spraying);
+    }
+}
