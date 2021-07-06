65fe36a00525a31aa1daec45fd44f8e0c8fec5eb
Asus <193387@student.upm.edu.my>
2021-07-06 18:28:23 +0800

    done coding utk btn Land_Preparation from data_input to land_preparation

---
+++ app/src/main/java/com/example/smartmanagingapps/harvesting.java
@@ -0,0 +1,14 @@
+package com.example.smartmanagingapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class harvesting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harvesting);
+    }
+}
